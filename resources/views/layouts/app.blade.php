06381ad1502c7a001d11d7f391c2f2a6ab1d9d7c
Junianto Endra <junianto.endra.21@gmail.com>
2019-10-04 16:34:19 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Larashop @yield("title")</title>
-  <link rel="stylesheet" href="{{asset('polished/polished.min.css')}}">
-  <link rel="stylesheet" href="{{asset('polished/iconic/css/open-iconic-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('public/polished/polished.min.css')}}">
+  <link rel="stylesheet" href="{{asset('public/polished/iconic/css/open-iconic-bootstrap.min.css')}}">
 
   <style>
     .grid-highlight {
